e2a19232015e9cdeaf17167882a43c756d030c51
robsouthgate4 <robsouthgate4@gmail.com>
2020-02-10 15:18:08 +0000

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,7 @@ module.exports = env => {
         output: {
             filename: '[name].bundle.js',
             path: path.resolve(__dirname, 'docs/'),
-            publicPath: env.NODE_ENV === 'production' ? '/' : ''
+            publicPath: env.NODE_ENV === 'production' ? '' : ''
         },
         module: {
             rules: [
