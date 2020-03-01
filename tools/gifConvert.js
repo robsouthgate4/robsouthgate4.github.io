6ead8b13aecf341140e9a8037ba1c48125207a28
robsouthgate4 <robsouthgate4@gmail.com>
2020-03-01 11:14:05 +0000

    Added new lab post

--- tools/gifConvert.js
+++ tools/gifConvert.js
@@ -1,6 +1,6 @@
 const gify = require('gify');
 
-gify('../src/assets/videos/lab/compressedx2/bubble.mp4', '../src/assets/videos/lab/gif_small/bubble.gif', {
+gify('../src/assets/videos/lab/compressed/motionFlow.mp4', '../src/assets/videos/lab/gif_small/motionFlow.gif', {
     duration: 5,
     width: 500
 }, function(err){
