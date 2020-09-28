785e2f2dc8292265426f9e4377dce2753a81957a
robsouthgate4 <robsouthgate4@gmail.com>
2020-09-28 15:30:48 +0100

    Added flocking to lab

--- tools/compress.js
+++ tools/compress.js
@@ -1,10 +1,12 @@
 const hbjs = require('handbrake-js');
  
 const options = {
-  input: '../src/assets/videos/portfolio/fifa.MOV',
-  output: '../src/assets/videos/portfolio/fifa.mp4',
+  input: '../src/assets/videos/lab/swarm.mp4',
+  output: '../src/assets/videos/lab/compressed/swarm.mp4',
   optimize: true,
-  quality: 30
+  crop:  [170, 0, 0, 0 ],
+  quality: 25,
+  width: 720
 }
 hbjs.spawn(options)
   .on('error', console.error)
